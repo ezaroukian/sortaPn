340cf8231a571f17eb7972e623fd662549b5720f
ezaroukian <egz@jhu.edu>
2015-07-21 09:26:49 -0400

    Change handle label

--- js_includes/Scale.js
+++ js_includes/Scale.js
@@ -251,7 +251,7 @@ jqueryWidget: {
         this.$handle.css('left', hleft + 'px');
         this.$handle.css('top', htop + 'px');
         //this.$handleLabel.text(this.fraction.toFixed(this.decimalPlaces));
-		this.$handleLabel.text(((this.endValue-this.startValue)*this.fraction+this.startValue).toFixed(this.decimalPlaces));
+		this.$handleLabel.text(((this.endValue-this.startValue)*this.fraction+this.startValue).toFixed(this.decimalPlaces));//to display the right value
         this.$handleLabel.css('left', parseInt(hleft + this.handleWidth/2 - this.$handleLabel.width()/2) + 'px');
         this.$handleLabel.css('top', parseInt(htop - this.handleHeight) + 'px');
         // Set color for handle.
